f97a6b09be5c3a01d2b50f4f3c69231fde24ec04
Carl Mungazi <carlmungazi@Carls-MacBook-Pro.local>
2019-11-08 01:31:14 +0000

    Refactor button animation + add tachyon locally

--- src/features/explainer/index.js
+++ src/features/explainer/index.js
@@ -5,7 +5,7 @@ import "./index.css";
 
 function Explainer() {
   return (
-    <Card className="br3 pa3 pa4-ns mv3 ba b--black-10">
+    <Card className="br3 pa3 pa4-ns mv3 ba b--black-10 bg-white">
       This is where the text housing the API explanation will live.
     </Card>
   );
--- src/features/search/index.js
+++ src/features/search/index.js
@@ -5,25 +5,29 @@ import { Input, Button } from "components";
 
 function SearchInput() {
   const [animationClass, toggleClass] = useState("search");
+  const [inputValue, setInputValue] = useState('')
   return (
     <div className={animationClass}>
       <Input
         type="search"
-        className="search-input"
-        placeholder="Search"
+        className="bg-light-red white placeholder-white bn search-input"
+        placeholder="What are you searching for today?"
         id="search"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
       />
       <Button
-        className="search-button"
+        className="bg-light-red bn search-button"
         onClick={() => {
-          const str = animationClass.search("search-open");
-          if (str === -1) return toggleClass("search search-open");
-
+          const str = animationClass.search("search-open ");
+          if (str === -1) return toggleClass("search search-open ");
+          setInputValue('');
+          
           return toggleClass("search");
         }}
         type="button"
       >
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 5 5">
+        <svg className="fill-blue" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 5 5">
           <path d="M2 1 h1 v1 h1 v1 h-1 v1 h-1 v-1 h-1 v-1 h1 z" />
         </svg>
       </Button>

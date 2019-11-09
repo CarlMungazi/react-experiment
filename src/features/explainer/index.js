2e823d27554530f252f9e264cf08dcd2da9b2071
Carl Mungazi <carlmungazi@Carls-MacBook-Pro.local>
2019-11-09 21:34:17 +0000

    Refactor classnames

--- src/features/explainer/index.js
+++ src/features/explainer/index.js
@@ -6,7 +6,9 @@ import "./index.css";
 function Explainer() {
   return (
     <Card className="br3 pa3 pa4-ns mv3 ba b--black-10 bg-white">
-      This is where the text housing the API explanation will live.
+      <p className="ma0">
+        This is where the text for the API explanation will live.
+      </p>
     </Card>
   );
 }
--- src/features/search/index.js
+++ src/features/search/index.js
@@ -1,33 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, createRef } from "react";
 import "./index.css";
 
 import { Input, Button } from "components";
 
 function SearchInput() {
-  const [animationClass, toggleClass] = useState("search");
-  const [inputValue, setInputValue] = useState('')
+  const [inputValue, setInputValue] = useState("");
+  const divRef = createRef();
+
   return (
-    <div className={animationClass}>
+    <div className="w-100 mt3 flex justify-center h3 search" ref={divRef}>
       <Input
         type="search"
-        className="bg-light-red white placeholder-white bn search-input"
+        className="bg-light-red white placeholder-white bn outline-0 h-100 ma0 z-10 f-20 trans-o-placeholder trans-duration-placeholder trans-duration search-input"
         placeholder="What are you searching for today?"
         id="search"
         value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
+        onChange={e => setInputValue(e.target.value)}
       />
       <Button
-        className="bg-light-red bn search-button"
+        className="bg-light-red bn h-100 db pa0 ma0 tc pointer outline-0 minw-60 search-button"
+        // eslint-disable-next-line
         onClick={() => {
-          const str = animationClass.search("search-open ");
-          if (str === -1) return toggleClass("search search-open ");
-          setInputValue('');
-          
-          return toggleClass("search");
+          setInputValue("");
+
+          if (divRef.current.classList.contains("search")) {
+            return divRef.current.classList.toggle("search--open");
+          }
+
+          divRef.current.classList.toggle("search");
         }}
         type="button"
       >
-        <svg className="fill-blue" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 5 5">
+        <svg
+          className="fill-blue trans-transform trans-duration"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 5 5"
+        >
           <path d="M2 1 h1 v1 h1 v1 h-1 v1 h-1 v-1 h-1 v-1 h1 z" />
         </svg>
       </Button>
